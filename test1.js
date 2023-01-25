17f94ccf797d5022c3f4cdc4844ba19f058fe742
Anelya <anel.bulatova7@gmail.com>
2023-01-25 11:54:31 +0300

    15

--- test1.js
+++ test1.js
@@ -81,3 +81,25 @@ function functionWithArgs(one, two) {
   }
   
   const answer = timesFive(5);
+
+  // Declare the myGlobal variable below this line
+
+
+function fun1() {
+  // Assign 5 to oopsGlobal here
+  
+}
+let  myGlobal = 10;
+let oopsGlobal = 5; 
+// Only change code above this line
+
+function fun2() {
+  let output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
