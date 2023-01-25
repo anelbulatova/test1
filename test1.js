a035a4374c1c3272277282cf7711316a47bac245
Anelya <anel.bulatova7@gmail.com>
2023-01-25 11:36:33 +0300

    12

--- test1.js
+++ test1.js
@@ -68,3 +68,7 @@ myArray.shift();
 // Only change code below this line
 myArray.unshift(["Paul",35]);
 var myList = [["Chocolate Bar", 15],["bread", 1], ["jam",2] ,["eggs", 10],["butter",2]];
+function reusableFunction() {
+  console.log("Hi World");
+}
+reusableFunction();
