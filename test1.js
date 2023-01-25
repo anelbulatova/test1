1161a64755328ad4eb81536de8ba21b26eef61a6
Anelya <anel.bulatova7@gmail.com>
2023-01-25 11:10:52 +0300

    8

--- test1.js
+++ test1.js
@@ -44,3 +44,8 @@ const myArray = [
 
 const myData = myArray[2][1];
 console.log(myData);
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+myArray.push(["dog", 3]);
