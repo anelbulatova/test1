37f46bf5511e4f837e29f71447ea6312d6c1e1b6
Anelya <anel.bulatova7@gmail.com>
2023-01-26 13:11:51 +0300

    11

--- test1.js
+++ test1.js
@@ -233,3 +233,17 @@ function testGreaterThan(val) {
 }
 
 console.log(testGreaterThan(10));
+
+function testGreaterOrEqual(val) {
+  if (val >= 20) {  // Change this line
+    return "20 or Over";
+  }
+
+  if (val >= 10) {  // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+console.log(testGreaterOrEqual(10));
