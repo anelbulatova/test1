ec8c39aab39c749b0d4a6bf76a24edf9c1023d01
Anelya <anel.bulatova7@gmail.com>
2023-01-26 13:02:14 +0300

    6

--- test1.js
+++ test1.js
@@ -172,3 +172,12 @@ if (wasThatTrue) {
   return "No, that was false";
 }
 
+// Setup
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+console.log(testEqual(10));
