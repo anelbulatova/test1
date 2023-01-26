d789b47740495b6122bc505e7c07588b90bdd53a
Anelya <anel.bulatova7@gmail.com>
2023-01-26 12:44:18 +0300

    2

--- test1.js
+++ test1.js
@@ -114,3 +114,20 @@ function myOutfit() {
 
 console.log(myOutfit());
 console.log(outerWear);
+
+// Setup
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+  sum = sum + 5
+}
+
+// Only change code above this line
+
+addThree();
+addFive(); 
