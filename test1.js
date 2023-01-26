03469ae95f45a6b333c1d49589d2e46305c4d667
Anelya <anel.bulatova7@gmail.com>
2023-01-26 13:07:02 +0300

    8

--- test1.js
+++ test1.js
@@ -200,3 +200,13 @@ function compareEquality(a, b) {
 }
 
 console.log(compareEquality(10, "10"));
+
+// Setup
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+console.log(testNotEqual(10));
