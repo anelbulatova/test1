b2e755e58bc27f3c103e601a744c6d5fba700118
Anelya <anel.bulatova7@gmail.com>
2023-01-28 23:43:14 +0300

    1

--- test1.js
+++ test1.js
@@ -275,3 +275,16 @@ function testLessOrEqual(val) {
 }
 
 console.log(testLessOrEqual(10));
+
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val < 10 || val > 20) {
+    return "Outside";
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
+
+testLogicalOr(15);
