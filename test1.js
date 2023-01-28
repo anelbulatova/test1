f18b502152f179a0e1a7ac643fe0a0350e3d7e69
Anelya <anel.bulatova7@gmail.com>
2023-01-28 23:59:26 +0300

    4

--- test1.js
+++ test1.js
@@ -324,3 +324,15 @@ function orderMyLogic(val) {
 }
 
 console.log(orderMyLogic(3));
+function testSize(num) {
+  if (num < 5) {
+    return "Tiny"
+  } else if (num < 10) {
+    return "Small"
+  } else if (num < 15) {
+    return "Medium"
+  } else if (num < 20) {
+    return "Large"
+  } else {
+    return "Huge"
+  }
